ba0b26ff0115a0f0a5c2ea62b0e0c55406982302
Chris Hayen <chayen@gmail.com>
2016-05-20 15:56:19 -0400

    first commit

---
+++ gitmake.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+import "strings"
+import "os"
+import "os/exec"
+import "golang.org/x/oauth2"
+import "github.com/google/go-github/github"
+
+func main() {
+  name := getName()
+  client := getClient()
+  createRepo(client, name)
+}
+
+func getName() string {
+  dir, _ := os.Getwd()
+  parts := strings.Split(dir, "/")
+  lastIndex := len(parts) - 1
+  name := parts[lastIndex]
+  return name
+}
+
+func getClient() *github.Client {
+  // Load oauth
+  token := os.Getenv("GIT_TOKEN")
+  ts:= oauth2.StaticTokenSource(
+    &oauth2.Token{AccessToken: token},
+  )
+
+  // Authenticate
+  tc:= oauth2.NewClient(oauth2.NoContext, ts)
+  client := github.NewClient(tc)
+
+  return client
+}
+
+func createRepo(client * github.Client, name string) bool {
+  repo := github.Repository{Name: &name}
+  _, _, err := client.Repositories.Create("", &repo)
+
+  if err == nil {
+    fmt.Println("Created repo", name)
+  } else {
+    fmt.Println("There was an error creating", name)
+    fmt.Println(err)
+  }
+
+  return err == nil
+}
